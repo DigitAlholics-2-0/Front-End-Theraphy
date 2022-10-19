7e053d19da41250ae5ec88d21f12da34c721fd9b
Luis Pineda <u20201e705@upc.edu.pe>
2022-10-19 00:27:29 -0500

    feat(home_physiotherapist): home for physiotherapist added

---
+++ src/app/user-type/doctor/pages/home2/home2.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {Physiotherapist} from "../../../patient/model/physiotherapist";
+import {Treatment} from "../../../patient/model/treatment";
+import {PhysiotherapistsService} from "../../../patient/services/physiotherapists.service";
+import {TreatmentsService} from "../../../patient/services/treatments.service";
+import {Patient} from "../../model/patient";
+import {PatientsService} from "../../services/patients.service";
+import {Appointments} from "../../../../shared/model/appointments";
+import {AppointmentsService} from "../../../../shared/services/appointments.service";
+
+@Component({
+  selector: 'app-home2',
+  templateUrl: './home2.component.html',
+  styleUrls: ['./home2.component.css']
+})
+export class Home2Component implements OnInit {
+  physiotherapists: Physiotherapist[]=[];
+  treatments: Treatment[]=[];
+  patients: Patient[] = [];
+  appointments: Appointments[]=[];
+  myPatients: Patient[]=[];
+  currentUser: number = 1;
+  aea:number = 0;
+
+  constructor(private physiotherapistsService: PhysiotherapistsService, private treatmentsService: TreatmentsService,
+              private patientsService: PatientsService, private appointmentsService: AppointmentsService) { }
+
+  ngOnInit(): void {
+    this.getAllPhysiotherapists();
+    this.getAllTreatments();
+    this.getAllPatients();
+    this.getAllAppointments();
+  }
+
+  getAllPhysiotherapists(){
+    this.physiotherapistsService.getAll().subscribe((response: any) =>{
+      this.physiotherapists = response;
+    })
+  }
+
+  getAllTreatments(){
+    this.treatmentsService.getAll().subscribe((response:any)=>{
+      this.treatments = response;
+    })
+    this.getMyPatients();
+
+  }
+
+  getAllPatients(){
+    this.patientsService.getAll().subscribe((response: any)=>{
+      this.patients=response;
+    })
+  }
+
+  getAllAppointments() {
+    this.appointmentsService.getAll().subscribe((response: any)=>{
+      this.appointments=response;
+    })
+  }
+
+  getMyPatients() {
+    this.appointments.forEach(element => {
+      if(this.currentUser == element.physiotherapist_id) {
+        this.patients.forEach(element2 => {
+          if(element2.id == element.patient_id) {
+            this.myPatients.push(element2);
+          }
+        })
+      }
+    })
+
+  }
+}
