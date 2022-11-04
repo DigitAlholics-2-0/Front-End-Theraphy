9b45a3c6cf91e44fdef6a7115f40460bcd088d63
Diego Osorio <diegosorioupc@gmail.com>
2022-11-04 00:12:07 -0500

    add: create my treatments for doctr

---
+++ src/app/user-type/doctor/pages/my-treatments/my-treatments.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Treatment} from "../../../../treatments/model/treatment";
+import {TreatmentsService} from "../../../../treatments/services/treatments.service";
+
+
+@Component({
+  selector: 'app-my-treatments',
+  templateUrl: './my-treatments.component.html',
+  styleUrls: ['./my-treatments.component.css']
+})
+export class MyTreatmentsComponent implements OnInit {
+
+  treatments: Treatment[]=[];
+  currentUsersTreatments: any = [1, 2, 3];
+
+  constructor(private treatmentsService: TreatmentsService) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.getAllTreatments();
+  }
+
+  getAllTreatments(){
+    this.treatmentsService.getAll().subscribe((response:any)=>{
+      this.treatments = response;
+    })
+  }
+
+}
